6deeaa803643f16f0b753adc2a560f58f64bbd66
madamnut <61892100+madamnut@users.noreply.github.com>
2026-04-23 22:49:38 +0900

    reset
    - 꼬여서 다시시작

--- references/noise/FastSIMD/include/FastSIMD/ToolSet/ARM/ARM.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#if FASTSIMD_MAX_FEATURE_VALUE() >= FASTSIMD_FEATURE_VALUE( NEON )
-#include "NEON.h"
-#endif
--- references/noise/FastSIMD/include/FastSIMD/ToolSet/x86/x86.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#if FASTSIMD_MAX_FEATURE_VALUE() >= FASTSIMD_FEATURE_VALUE( SSE )
-#include "SSE.h"
-#endif
